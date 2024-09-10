72c06a56772553d3ea32916f058495c080bc465f
dhp82 <42089795+dhp82@users.noreply.github.com>
2024-09-10 12:57:11 +0700

    Update install.sh

--- LAN/install.sh
+++ LAN/install.sh
@@ -37,22 +37,6 @@ install_3proxy() {
     cd $WORKDIR
 }
 
-install_3proxy2() {
-    echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
-    systemctl stop firewalld
-    systemctl disable firewalld
-    cd $WORKDIR
-}
-
 gen_3proxy() {
     cat <<EOF
 daemon

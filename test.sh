3300f751584a783f41c93c3ba3ac2b918d0cd09f
dhp82 <42089795+dhp82@users.noreply.github.com>
2024-12-01 14:28:47 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
@@ -28,24 +28,15 @@ install_3proxy() {
     mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
     wget https://raw.githubusercontent.com/dhp82/3proxy-alma/main/LAN/3proxy.service-almalinux8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
     cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+	#cp /root/3proxy.service-almalinux8 /usr/lib/systemd/system/3proxy.service
     systemctl link /usr/lib/systemd/system/3proxy.service
     systemctl daemon-reload
-    echo "* hard nofile 999999" >>/etc/security/limits.conf
-    echo "* soft nofile 999999" >>/etc/security/limits.conf
-}
+    echo "* hard nofile 999999" >>  /etc/security/limits.conf
+    echo "* soft nofile 999999" >>  /etc/security/limits.conf
+    #systemctl stop firewalld
+    #systemctl disable firewalld
 
-setup_nat64() {
-    echo "Configuring NAT64..."
-    # Bật forwarding IPv6 và IPv4
-    sysctl -w net.ipv6.conf.all.forwarding=1
-    sysctl -w net.ipv4.conf.all.forwarding=1
-
-    # Ánh xạ IPv6 ra IPv4 (sử dụng ip6tables để cấu hình NAT64)
-    ip6tables -t nat -A POSTROUTING -o ${main_interface} -j MASQUERADE
-
-    # Ánh xạ các yêu cầu IPv6 ra IPv4, sử dụng địa chỉ NAT64 (mạng IPv6 của bạn sẽ kết nối với địa chỉ IPv4 bên ngoài)
-    ip6tables -t nat -A POSTROUTING -o ${main_interface} -s 64:ff9b::/96 -d 0.0.0.0/0 -j MASQUERADE
-    iptables -t nat -A PREROUTING -d 0.0.0.0/0 -p tcp --dport 80 -j DNAT --to-destination 64:ff9b::/96
+    cd $WORKDIR
 }
 
 gen_3proxy() {
@@ -60,18 +51,20 @@ nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456
+stacksize 6291456 
 flush
 
-$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\nflush\n"}' ${WORKDATA})
+$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $1 ":" $2}' ${WORKDATA})
+$(awk -F "/" '{print $1 ":" $2 }' ${WORKDATA})
 EOF
 }
+
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
@@ -79,13 +72,18 @@ gen_data() {
     done
 }
 
-cat <<EOF >/etc/rc.d/rc.local
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $3 "/64"}' ${WORKDATA})
+EOF
+}
+
+cat << EOF > /etc/rc.d/rc.local
 #!/bin/bash
 touch /var/lock/subsys/local
 EOF
 
 install_3proxy
-setup_nat64
 
 WORKDIR="/home/dhp82"
 WORKDATA="${WORKDIR}/data.txt"
@@ -96,24 +94,32 @@ IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-3 -d':')  # Lấy subnet /48
 
 echo "LAN IP = ${IP_LAN}"
-echo "Public IPv4 = ${IP4}"
+#echo "Public IPv4 = ${IP4}"
 echo "Subnet for IPv6 = ${IP6}"
 
-COUNT=10 # Số lượng proxy
-FIRST_PORT=10000
+#echo "NHAP SO LUONG PROXY MUON TAO"
+#read COUNT
+COUNT=10
+FIRST_PORT=28282
 LAST_PORT=$(($FIRST_PORT + $COUNT))
+#LAST_PORT=$(($FIRST_PORT + XXX))
 
 echo "gen data..."
 gen_data >$WORKDIR/data.txt
+echo "gen ifconfig..."
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 systemctl start NetworkManager.service
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
 
 bash /etc/rc.local
+
 gen_proxy_file_for_user
 echo "Starting Proxy"

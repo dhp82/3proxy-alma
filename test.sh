9149b40b194e518442d639fdc5a438d8ee54aff7
dhp82 <42089795+dhp82@users.noreply.github.com>
2024-12-01 14:02:40 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+
+gen128() {
+    # Random từ octet 4 đến octet 8
+    ip_part() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip_part):$(ip_part):$(ip_part):$(ip_part):$(ip_part)"
+}
+
+install_3proxy() {
+    echo "installing 3proxy"
+    mkdir -p /3proxy
+    cd /3proxy
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.3
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/dhp82/3proxy-alma/main/LAN/3proxy.service-almalinux8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    systemctl link /usr/lib/systemd/system/3proxy.service
+    systemctl daemon-reload
+    echo "* hard nofile 999999" >>/etc/security/limits.conf
+    echo "* soft nofile 999999" >>/etc/security/limits.conf
+}
+
+setup_nat64() {
+    echo "Configuring NAT64..."
+    sysctl -w net.ipv6.conf.all.forwarding=1
+    sysctl -w net.ipv4.conf.all.forwarding=1
+    ip6tables -t nat -A POSTROUTING -o ${main_interface} -j MASQUERADE
+}
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 4000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456
+flush
+
+$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\nflush\n"}' ${WORKDATA})
+EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $1 ":" $2}' ${WORKDATA})
+EOF
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$IP_LAN/$port/$(gen128 $IP6)"
+    done
+}
+
+cat <<EOF >/etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
+
+install_3proxy
+setup_nat64
+
+WORKDIR="/home/dhp82"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP_LAN="x.x.x.x"
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-3 -d':')  # Lấy subnet /48
+
+echo "LAN IP = ${IP_LAN}"
+echo "Public IPv4 = ${IP4}"
+echo "Subnet for IPv6 = ${IP6}"
+
+COUNT=1000 # Số lượng proxy
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+echo "gen data..."
+gen_data >$WORKDIR/data.txt
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+systemctl start NetworkManager.service
+ulimit -n 65535
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
+EOF
+
+bash /etc/rc.local
+gen_proxy_file_for_user
+echo "Starting Proxy"

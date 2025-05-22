60ed9fa956c71502556d9da1822ad1b5af32bcc7
dhp82 <42089795+dhp82@users.noreply.github.com>
2025-05-22 23:23:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ gen_data() {
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
